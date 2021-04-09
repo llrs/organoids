a4ae52aa2c997eb8c1ef1cc141f1fec4ac4132dc
llrs <lluis.revilla@gmail.com>
2021-04-09 11:19:07 +0200

    RankProd is not useful
    As decided on 2021/04/08

--- R/04_comparatives.R
+++ R/04_comparatives.R
@@ -47,62 +47,6 @@ res <- multilimma(counts, comp_01, "cyclicloess", meta2$SAMPLE)
 saveRDS(res, "data_out/limma_juanjo.RDS")
 tr <- make_TestResults(res) # Transform to limma TestResults object to see a summary
 
-mRP <- multiRankProd(counts, comp_01, "cyclicloess", meta2$SAMPLE)
-saveRDS(mRP, "data_out/rankprod_paired.RDS")
-
-genes <- strsplit(rownames(mRP[[1]][[1]]), "\\.[0-9]+_")
-genes <- t(simplify2array(genes))
-colnames(genes) <- c("ENSEMBL", "name")
-res_RP <- lapply(mRP, function(x) {
-  y <- x$pval
-  colnames(y) <- paste0("p.val_", colnames(y))
-  y2 <- apply(y, 2, p.adjust, method = "fdr")
-  colnames(y2) <- paste0("fdr_", colnames(y2))
-  o <- cbind(x$AveFC, y2, y)
-  colnames(o)[1] <- "fc_"
-  o
-  })
-cond <- paste0(sub_compar$`Referencia comparativa`, "_vs_",
-               sub_compar$`Variable comparativa`)
-for (i in seq_along(res_RP)) {
-  colnames(res_RP[[i]]) <- paste0("c",sub_compar$`Número comparativa`[i], "_", colnames(res_RP[[i]]), cond[i])
-}
-sign_fc <- fc <- matrix(ncol = length(res_RP), nrow = nrow(res_RP[[1]]))
-p.val <- p.adj <- matrix(ncol = length(res_RP)*2, nrow = nrow(res_RP[[1]]))
-
-colnames(fc) <- seq_len(ncol(fc))
-colnames(sign_fc) <- seq_len(ncol(sign_fc))
-colnames(p.val) <- seq_len(ncol(p.val))
-colnames(p.adj) <- seq_len(ncol(p.adj))
-for (i in seq_along(res_RP)) {
-  fc[, i] <- res_RP[[i]][, 1]
-  colnames(fc)[i] <- colnames(res_RP[[i]])[1]
-  sign_fc[, i] <- ifelse(res_RP[[i]][, 4] < 0.05 & res_RP[[i]][, 5] > 0.05, "UP",
-                         ifelse(res_RP[[i]][, 5] < 0.05 & res_RP[[i]][, 4] > 0.05, "DW",
-                                ifelse(res_RP[[i]][, 2] < 0.05, "UUP",
-                                       ifelse(res_RP[[i]][, 3] < 0.05, "DW", NA))))
-  colnames(sign_fc)[i] <- gsub("_fc_", "_sign_", colnames(res_RP[[i]])[1])
-  # The strange i*2 is to account for 2 columns
-  cols <- c(i*2 - 1, i*2)
-  p.adj[, cols] <- res_RP[[i]][, 2:3]
-  colnames(p.adj)[cols] <- colnames(res_RP[[i]])[2:3]
-  p.val[, cols] <- res_RP[[i]][, 4:5]
-  colnames(p.val)[cols] <- colnames(res_RP[[i]])[4:5]
-}
-
-o <- cbind.data.frame(genes, sign_fc, fc, genes)
-write.table(o, file = "data_out/GETS_gene_RankProd.tsv", quote = FALSE, sep = "\t", row.names = FALSE, na = "")
-system2("gzip", args = "-kf data_out/GETS_gene_RankProd.tsv") # Compress it to upload to website
-
-# summary(tr)
-# res_quantile <- multilimma(counts, comp_01, "quantile", meta2$SAMPLE)
-# saveRDS(res_quantile, "data_out/limma_paired_quantile.RDS")
-#
-# res_unparied_cyclicloess <- multilimma(counts, comp_01, "cyclicloess")
-# saveRDS(res_unparied_cyclicloess, "data_out/limma_unpaired_cyclicloess.RDS")
-# res_unparied_quantile <- multilimma(counts, comp_01, "quantile")
-# saveRDS(res_unparied_quantile, "data_out/limma_unpaired_quantile.RDS")
-
 # GETS ####
 # * Create matrix for significance ####
 diff <- matrix(dimnames = dimnames(res$fc), ncol = ncol(res$fc), nrow = nrow(res$fc))
@@ -110,7 +54,6 @@ diff[res$p < 0.05 & res$fc > 0] <- "UP"
 diff[res$p < 0.05 & res$fc < 0] <- "DW"
 diff[res$fdr < 0.05 & res$fc > 0] <- "UUP"
 diff[res$fdr < 0.05 & res$fc < 0] <- "DDW"
-
 
 colnames(diff) <- gsub("fc_", "sign_", colnames(diff))
 
