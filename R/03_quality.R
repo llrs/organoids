2581add5e1a52de16aaf8ac68137df3ec68ff31a
llrs <lluis.revilla@gmail.com>
2021-05-03 12:08:37 +0200

    More PCAs for QA

--- R/03_quality.R
+++ R/03_quality.R
@@ -7,6 +7,7 @@ library("dplyr")
 library("stringr")
 library("integration")
 library("biomaRt")
+library("sva")
 source("R/02_functions_juanjo.R")
 
 tx <- readRDS("data/amayorgas_organoids_counts.RDS")
@@ -52,7 +53,11 @@ png("Figures/library_size.png")
 barplot(log10(sort(colSums(counts3))))
 dev.off()
 
-pdf("Figures/PCAs.pdf")
+batch <- ifelse(meta2$SAMPLE == "604", "surgery", "biopsy")
+mod <- model.matrix(~cond + SAMPLE,  meta2)
+sva_combat <- sva::ComBat(counts4, batch = batch, mod = mod)
+
+pdf("Figures/PCAs_new.pdf")
 plotPCA(t(counts4), meta2$cond)
 plotPCA(t(counts4), ifelse(!is.na(meta2$estimul), "Estímul", "No estímul"))
 plotPCA(t(counts4), meta2$SAMPLE)
@@ -61,4 +66,53 @@ data.pca <- prcomp(t(counts4), scale. = TRUE)
 ggbiplot(data.pca, groups = as.character(meta2$SAMPLE), var.axes = FALSE, ellipse = TRUE, circle = FALSE,
          labels = colnames(counts4)) +
   theme_minimal()
+
+plotPCA(t(sva_combat), meta2$cond) + labs(title = "Without sample effect")
+plotPCA(t(sva_combat), ifelse(!is.na(meta2$estimul), "Estímul", "No estímul"))  + labs(title = "Without sample effect")
+
+plotPCA(t(sva_combat), as.character(meta2$SAMPLE)) + labs(title = "Without sample effect")
+plotPCA(t(sva_combat), meta2$PB) + labs(title = "Without sample effect")
+data.pca <- prcomp(t(sva_combat), scale. = TRUE)
+ggbiplot(data.pca, groups = as.character(meta2$SAMPLE), var.axes = FALSE, ellipse = TRUE, circle = FALSE,
+         labels = colnames(sva_combat)) +
+  theme_minimal() + labs(title = "Without sample effect")
+dev.off()
+
+# Adding PCAs 2021/05/03 based on TO_DO Potsti2D_bioinfo
+tsc <- t(sva_combat)
+meta2$PBS <- grepl("PBS$", meta2$cond)
+
+pdf("Figures/PCAs_PBS_estímul_PBS.pdf")
+keep <- !is.na(meta2$estimul) & meta2$PBS
+plotPCA(tsc[keep, ], meta2$cond[keep]) + labs(title = "Without sample effect")
+meta3 <- meta2[keep, ]
+meta3$name <- meta3$`Macrogen SAMPLE NAME`
+
+for (e in unique(meta3$estimul)) {
+  keep2 <- meta3$estimul == e | meta3$estimul == "PBS"
+  if (e == "PBS") {
+    next
+  }
+  a <- plotPCA(tsc[meta3$name[keep2], ], meta3$cond[keep2]) +
+    labs(title = paste("Without sample effect", e),
+         col = "Condició")
+  print(a)
+}
+dev.off()
+
+pdf("Figures/PCAs_PB_estímul_PB.pdf")
+keep <- !meta2$PBS & (!is.na(meta2$PB) | !is.na(meta2$estimul))
+plotPCA(tsc[keep, ], meta2$cond[keep]) + labs(title = "Without sample effect")
+meta3 <- meta2[keep, ]
+meta3$name <- meta3$`Macrogen SAMPLE NAME`
+
+for (e in unique(meta3$estimul)) {
+  keep2 <- meta3$estimul %in% e
+  if (e == "PBS") {
+    next
+  }
+  a <- plotPCA(tsc[meta3$name[keep2], ], meta3$cond[keep2]) +
+    labs(title = paste("Without sample effect, ", e), col = "Condició")
+  print(a)
+}
 dev.off()
